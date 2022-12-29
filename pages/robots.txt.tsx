22ff9aca01cc332cb342edfc5690976988182d78
Martyn <zogacc@gmail.com>
2022-12-29 19:31:59 +0400

    upgrade next-notion-starter

--- pages/robots.txt.tsx
+++ pages/robots.txt.tsx
@@ -1,31 +1,45 @@
-import { GetServerSideProps } from 'next'
-import { host } from 'lib/config'
+import type { GetServerSideProps } from 'next'
+
+import { host } from '@/lib/config'
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   if (req.method !== 'GET') {
     res.statusCode = 405
-    res.setHeader("Content-Type", "application/json")
-    res.write(JSON.stringify({ error: "method not allowed" }))
+    res.setHeader('Content-Type', 'application/json')
+    res.write(JSON.stringify({ error: 'method not allowed' }))
     res.end()
+
     return {
       props: {}
     }
   }
 
-  res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=60, max-age=60, stale-while-revalidate=60'
-  )
+  // cache for up to one day
+  res.setHeader('Cache-Control', 'public, max-age=86400, immutable')
   res.setHeader('Content-Type', 'text/plain')
-  res.write(`User-agent: *
-  Sitemap: ${host}/sitemap.xml
-  `)
+
+  // only allow the site to be crawlable on the production deployment
+  if (process.env.VERCEL_ENV === 'production') {
+    res.write(`User-agent: *
+Allow: /
+Disallow: /api/get-tweet-ast/*
+Disallow: /api/search-notion
+
+Sitemap: ${host}/sitemap.xml
+`)
+  } else {
+    res.write(`User-agent: *
+Disallow: /
+
+Sitemap: ${host}/sitemap.xml
+`)
+  }
+
   res.end()
+
   return {
     props: {}
   }
 }
 
-const RobotsTxt: React.FC = () => null
-
-export default RobotsTxt
+export default () => null
